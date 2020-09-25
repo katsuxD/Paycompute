de7940414807494f74142c2447172cfeeac9791a
katsuxD <khanhb5192@gmail.com>
2020-09-25 14:36:09 +0700

    Initial Commit

--- Paycompute.Services/Implementation/EmployeeService.cs
+++ Paycompute.Services/Implementation/EmployeeService.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Paycomputer.Services.Implementation
+namespace Paycompute.Services.Implementation
 {
     public class EmployeeService : IEmployeeService
     {
