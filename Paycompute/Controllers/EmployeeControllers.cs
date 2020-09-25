17ba2511340e3296fcf1aa2558a47e7b0a37e219
katsuxD <khanhb5192@gmail.com>
2020-09-25 15:35:22 +0700

    Index - EmployeeController

---
+++ Paycompute/Controllers/EmployeeControllers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paycompute.Models;
+using Paycompute.Services;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeControllers : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeControllers(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.Fullname,
+                Gender = employee.Gender,
+                Designation = employee.Designation,
+                City = employee.City,
+                DateJoined = employee.DateJoined
+            }).ToList();
+            return View(employees);
+        }
+
+    }
+}
