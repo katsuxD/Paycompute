c6d2a4f596df5d2fb723c61e632de84b3bf2e240
katsuxD <khanhb5192@gmail.com>
2020-10-31 16:29:50 +0700

    Have no idea why the scaffold identity did not work so I decided to copy the identify folder which been scaffoled successfully to continute the project

---
+++ Paycompute.Persistence/DataSeedingInitializer.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Paycompute.Persistence
+{
+    public static class DataSeedingInitializer
+    {
+        public static async Task UserAndRoleSeedAsync(UserManager<IdentityUser> userManager, 
+                                                 RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles = { "Admin", "Manager", "Staff" };
+            foreach (var role in roles)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                {
+                    IdentityResult result = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            //Create Admin User
+            if (userManager.FindByEmailAsync("admin@nhom5.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "admin@nhom5.com",
+                    Email = "admin@nhom5.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+                if (identityResult.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+
+            //Create Manager User
+            if (userManager.FindByEmailAsync("manager@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "manager@gmail.com",
+                    Email = "manager@gmail.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+                if (identityResult.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Manager").Wait();
+                }
+            }
+
+            //Create Staff User
+            if (userManager.FindByEmailAsync("khanh.dang@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "khanh.dang@gmail.com",
+                    Email = "khanh.dang@gmail.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+                if (identityResult.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Staff").Wait();
+                }
+            }
+
+            //Create No Role User
+            if (userManager.FindByEmailAsync("dat.dinh@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "dat.dinh@gmail.com",
+                    Email = "dat.dinh@gmail.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+               //No Role assigned to Mr John Doe
+            }
+        }
+    }
+}
--- Paycompute/Startup.cs
+++ Paycompute/Startup.cs
@@ -65,7 +65,10 @@ namespace Paycompute
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env,
+                              UserManager<IdentityUser> userManager,
+                              RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -84,7 +87,7 @@ namespace Paycompute
             app.UseCookiePolicy();
 
             app.UseAuthentication();
-
+            DataSeedingInitializer.UserAndRoleSeedAsync(userManager, roleManager).Wait();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
